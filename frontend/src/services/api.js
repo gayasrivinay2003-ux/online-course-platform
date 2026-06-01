963843f0364e4f5ad5bce2b92929e9124877bccd
gayasrivinay2003-ux <gayasrivinay2003@gmail.com>
2026-06-01 20:55:16 +0530

    update

--- frontend/src/services/api.js
+++ frontend/src/services/api.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const API = axios.create({
-  baseURL: "http://localhost:5000/api",
+  baseURL: "https://online-course-platform-wvrx.onrender.com/api",
 });
 
 export default API;
